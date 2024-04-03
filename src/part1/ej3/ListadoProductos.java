536e40479094e6aafaaf1bffe3d56df08d7837a9
enieves <enieves@106-02.info2>
2024-04-03 10:08:44 +0200

    comments

---
+++ src/part1/ej3/ListadoProductos.java
@@ -0,0 +1,9 @@
+package part1.ej3;
+
+public class ListadoProductos {
+	
+	Producto[] productos=new Producto[10];
+	
+	
+	
+}
---
+++ src/part1/ej3/NoPerecedero.java
@@ -0,0 +1,12 @@
+package part1.ej3;
+
+public class NoPerecedero extends Producto{
+
+	String tipo;
+	
+	public NoPerecedero(String nombre, double precio) {
+		super(nombre, precio);
+		// TODO Auto-generated constructor stub
+	}
+
+}
