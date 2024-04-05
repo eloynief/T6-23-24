62411421fe1458d3450e145261aa0c0ad3d30c44
enieves <enieves@106-02.info2>
2024-04-05 13:41:08 +0200

    añadir cosas del main para añadir modificar etc

---
+++ src/part1/ej4/ListadoElectrodomesticos.java
@@ -0,0 +1,13 @@
+package part1.ej4;
+
+public class ListadoElectrodomesticos {
+	
+	//listado de electrodomesticos
+	public static Electrodomestico[] lista=new Electrodomestico[10];
+	
+	
+	
+	
+	
+	
+}
